bc01489c5159a7e42b939c1e1e38e368d040bd6b
liugogal <259394345@qq.com>
2018-04-09 23:00:08 +0800

    update: 修复feign版本差异带来的问题

--- client/src/main/java/com/huatuo/product/client/ProductClient.java
+++ client/src/main/java/com/huatuo/product/client/ProductClient.java
@@ -2,7 +2,7 @@ package com.huatuo.product.client;
 
 import com.huatuo.product.common.DecreaseStockInput;
 import com.huatuo.product.common.ProductInfoOutput;
-import org.springframework.cloud.netflix.feign.FeignClient;
+import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
